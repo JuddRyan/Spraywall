30b396b48d63048de7dd654f31e726ed4f8e6c04
JuddRyan <ryanjudd03@gmail.com>
2023-03-29 17:57:35 +0200

    added MUI appbar template

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 	RouterProvider,
 } from 'react-router-dom';
 import './App.css';
+import ResponsiveAppBar from './components/appbar';
 import CreateRoute from './pages/createRoute';
 
 function App() {
@@ -19,30 +20,17 @@ function App() {
 	);
 
 	return (
-		<div className="App">
+		<>
 			<RouterProvider router={router} />
-			{/* <ul>
-				<Link to="/">Home</Link>
-				<Link to="/createRoute">createRoute</Link>
-			</ul> */}
-		</div>
+		</>
 	);
 }
 
 const Root = () => {
 	return (
 		<>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/createRoute">createRoute</Link>
-				</li>
-			</ul>
-			<div>
-				<Outlet />
-			</div>
+			<ResponsiveAppBar></ResponsiveAppBar>
+			<Outlet />
 		</>
 	);
 };
